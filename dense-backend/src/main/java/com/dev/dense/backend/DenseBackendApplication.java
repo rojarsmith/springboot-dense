576ace5d264373c6d3d5dedcd93324f9dba5a691
Rojar Smith <rojarsmith@gmail.com>
2023-09-30 00:37:37 +0800

    Initial Project

---
+++ dense-backend/src/main/java/com/dev/dense/backend/DenseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dev.dense.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DenseBackendApplication.class, args);
+	}
+
+}
